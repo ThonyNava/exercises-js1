ebbd0ef1090349a9bbe251b66194a1c7787c2818
Thony Nava <41093760+ThonyNava@users.noreply.github.com>
2020-08-15 14:27:50 +0200

    Homework L

--- week-1/Homework/L-functions-nested/exercise.js
+++ week-1/Homework/L-functions-nested/exercise.js
@@ -3,3 +3,12 @@ var mentor2 = "Irina";
 var mentor3 = "Mimi";
 var mentor4 = "Rob";
 var mentor5 = "Yohannes";
+
+const mentors = [mentor1, mentor2, mentor3, mentor4, mentor5];
+
+mentors.forEach(greetings);
+
+function greetings(name) {
+  let greeting = `Hello ${name}`;
+  console.log(greeting.toUpperCase());
+}
